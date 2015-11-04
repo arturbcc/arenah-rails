615cb645922bcd3ae66319aaa3f8bedfeb156601
Artur <artur.prado@abril.com.br>
2015-11-04 13:34:09 -0200

    bower, bootstrap and jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require_tree .
+//= require jquery/dist/jquery.min
+//= require bootstrap/dist/js/bootstrap.min
