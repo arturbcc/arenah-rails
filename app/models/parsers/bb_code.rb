dfe62f948ee20e4768878a0860213bdf6ce420e1
Artur <artur.prado@abril.com.br>
2015-11-19 10:16:48 -0200

    BBCode parser with tests

--- app/models/parsers/bb_code.rb
+++ app/models/parsers/bb_code.rb
@@ -7,22 +7,6 @@ class Parsers::BBCode
 
   def self.additional_tags
     {
-      b: {
-        html_open: '<strong>', html_close: '</strong>',
-        description: 'Make text bold',
-        example: 'This is [b]bold[/b].'},
-      i: {
-        html_open: '<em>', html_close: '</em>',
-        description: 'Make text italic',
-        example: 'This is [i]italic[/i].'},
-      u: {
-        html_open: '<u>', html_close: '</u>',
-        description: 'Underline text',
-        example: 'This is [u]underlined[/u].'},
-      s: {
-        html_open: '<span style="text-decoration:line-through;">', html_close: '</span>',
-        description: 'Strike-through text',
-        example: 'This is [s]wrong[/s] good.'},
       kbd: {
         html_open: '<kbd>', html_close: '</kbd>',
         description: 'Highlight the text',
@@ -43,123 +27,51 @@ class Parsers::BBCode
         html_open: '<div style="text-align:justify; text-justify: inter-word;">', html_close: '</div>',
         description: 'Justify a text',
         example: '[justify]This is centered[/justify].'},
-      ul: {
-        html_open: '<ul>', html_close: '</ul>',
-        description: 'Unordered list',
-        example: '[ul][li]List item[/li][li]Another list item[/li][/ul].',
-        only_allow: [ :li, "*".to_sym ]},
       code: {
         html_open: '<pre>', html_close: '</pre>',
         description: 'Code block with mono-spaced text',
         example: 'This is [code]mono-spaced code[/code].'},
-      ol: {
-        html_open: '<ol>', html_close: '</ol>',
-        description: 'Ordered list',
-        example: '[ol][li]List item[/li][li]Another list item[/li][/ol].',
-        only_allow: [ :li, "*".to_sym ]},
-      li: {
-        html_open: '<li>', html_close: '</li>',
-        description: 'List item',
-        example: '[ul][li]List item[/li][li]Another list item[/li][/ul].',
-        only_in: [ :ul, :ol ]},
-      list: {
-        html_open: '<ul>', html_close: '</ul>',
-        description: 'Unordered list',
-        example: '[list][*]List item[*]Another list item[/list].',
-        only_allow: [ "*".to_sym ]},
-      "*".to_sym => {
-        html_open: '<li>', html_close: '</li>',
-        description: 'List item',
-        example: '[list][*]List item[*]Another list item[/list].',
-        self_closable: true,
-        only_in: [ :list, :ul, :ol ]},
       img: {
-        html_open: '<img src="%between%" %width%%height%alt="" />', html_close: '',
+        html_open: '<img src="%between%" %width%%height%/>', html_close: '',
         description: 'Image',
         example: '[img]http://www.google.com/intl/en_ALL/images/logo.gif[/img].',
         only_allow: [],
         require_between: true,
         allow_quick_param: true, allow_between_as_param: false,
         quick_param_format: /^(\d+)x(\d+)$/,
-        param_tokens: [{token: :width, prefix: 'width="', postfix: '" ', optional: true },
-                              { token: :height,  prefix: 'height="', postfix: '" ', optional: true } ],
+        param_tokens: [
+          { token: :width, prefix: 'width="', postfix: '" ', optional: true },
+          { token: :height,  prefix: 'height="', postfix: '" ', optional: true } ],
         quick_param_format_description: 'The image parameters \'%param%\' are incorrect, \'<width>x<height>\' excepted'},
-      url: {
-        html_open: '<a href="%url%">%between%', html_close: '</a>',
-        description: 'Link to another page',
-        example: '[url]http://www.google.com/[/url].',
-        only_allow: [],
-        require_between: true,
-        allow_quick_param: true, allow_between_as_param: true,
-        quick_param_format: /^((((http|https|ftp):\/\/)|\/).+)$/,
-        quick_param_format_description: 'The URL should start with http:// https://, ftp:// or /, instead of \'%param%\'',
-        param_tokens: [{ token: :url }]},
       quote: {
         html_open: '<blockquote>', html_close: '</blockquote>',
         description: 'Quote another person',
         example: '[quote]BBCode is great[/quote]'},
-      size: {
-        html_open: '<span style="font-size: %size%px;">', html_close: '</span>',
-        description: 'Change the size of the text',
-        example: '[size=32]This is 32px[/size]',
-        allow_quick_param: true, allow_between_as_param: false,
-        quick_param_format: /(\d+)/,
-        quick_param_format_description: 'The size parameter \'%param%\' is incorrect, a number is expected',
-        param_tokens: [{token: :size}]},
       color: {
-        html_open: '<span style="color: %color%;">', html_close: '</span>',
+        html_open: '<font color="%color%">', html_close: '</font>',
         description: 'Change the color of the text',
         example: '[color=red]This is red[/color]',
         allow_quick_param: true, allow_between_as_param: false,
         quick_param_format: /(([a-z]+)|(#[0-9a-f]{6}))/i,
         param_tokens: [{token: :color}]},
-      youtube: {
-        html_open: '<iframe id="player" type="text/html" width="%width%" height="%height%" src="http://www.youtube.com/embed/%between%?enablejsapi=1" frameborder="0"></iframe>', html_close: '',
-        description: 'YouTube video',
-        example: '[youtube]E4Fbk52Mk1w[/youtube]',
-        only_allow: [],
-        url_matches: [/youtube\.com.*[v]=([^&]*)/, /youtu\.be\/([^&]*)/, /y2u\.be\/([^&]*)/],
-        require_between: true,
-        param_tokens: [
-          { token: :width, optional: true, default: 400 },
-          { token: :height, optional: true, default: 320 }
-        ]},
-      vimeo: {
-        html_open: '<iframe src="http://player.vimeo.com/video/%between%?badge=0" width="%width%" height="%height%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>',
-        html_close: '',
-        description: 'Vimeo video',
-        example: '[vimeo]http://vimeo.com/46141955[/vimeo]',
-        only_allow: [],
-        url_matches: [/vimeo\.com\/([^&]*)/],
-        require_between: true,
-        param_tokens: [
-          { token: :width, optional: true, default: 400 },
-          { token: :height, optional: true, default: 320 }
-        ]},
-      media: {
-        multi_tag: true,
-        require_between: true,
-        supported_tags: [
-          :youtube,
-          :vimeo
-        ]
-      },
       master: {
         html_open: '<div class="bbcode-master"><i class="fa fa-comments fa-2x"></i>', html_close: '</div>',
         description: 'Send a hidden message to the game master',
         example: '[master]Can I change my attribute?[/master].'},
       dice: {
-        html_open: '<div class="bbcode-dice">%between%</div>', html_close: '',
+        html_open: '<div class="bbcode-dice"><font color="%color%">', html_close: '</font></div>',
         description: 'Show the result for a simple dice roll',
-        require_between: true,
-        example: 'dice result: [dice]1d100 = 10[/dice].'},
+        example: 'dice result: [dice]1d100 = 10[/dice].',
+        allow_quick_param: true,
+        allow_between_as_param: false,
+        quick_param_format: /(([a-z]+)|(#[0-9a-f]{6}))/i,
+        param_tokens: [{ token: :color, optional: true, default: '#000' }]},
       dices: {
         html_open: '<div class="bbcode-full-width"><div class="bbcode-dices"><kbd> %title%</kbd><hr/>%between%</div></div>', html_close: '',
         description: 'Show the complete result for a dice roll',
         require_between: true,
         example: 'dice result: [dices=Title]1d100 = 10[/dices].',
-        param_tokens: [{ token: :title, optional: true, prefix: 'width=', postfix: ''  }]
-      },
+        param_tokens: [{ token: :title, optional: true, prefix: 'title=', postfix: '', default: '' }]},
       line: {
         html_open: '<hr />', html_close: '',
         description: 'Make a line',
@@ -169,10 +81,10 @@ class Parsers::BBCode
         description: 'Css clear',
         example: '[clear][/clear].'},
       spoiler: {
-        html_open: '<span class="bbcode-spoiler"><a href="javascript:;"><i class="fa fa-plus-square"></i> %title%</a><span>%between%</span></span>', html_close: '',
+        html_open: '<span class="bbcode-spoiler"><a href="javascript:;"><i class="fa fa-plus-square"></i> %title%</a><span>', html_close: '</span></span>',
         description: 'Show a hidden spoiler',
-        example: '[spoiler=Title]Ele morre no final[/dices].',
-        param_tokens: [{ token: :title, optional: true }]
+        example: '[spoiler=Title]He dies in the end[/spoiler].',
+        param_tokens: [{ token: :title, optional: true, default: '' }]
       }
     }
   end
--- spec/models/parsers/bbcode_spec.rb
+++ spec/models/parsers/bbcode_spec.rb
@@ -24,9 +24,119 @@ describe Parsers::BBCode do
     it 'applies strike through' do
       expect(parse('I [s]hate[/s] love it')).to eq('I <span style="text-decoration:line-through;">hate</span> love it')
     end
+
+    it 'highlights a text' do
+      expect(parse('Press [kbd]ENTER[/kbd]')).to eq('Press <kbd>ENTER</kbd>')
+    end
+
+    it 'aligns the text' do
+      orientations = %W(left right center)
+      orientations.each do |orientation|
+        expect(parse("[#{orientation}]Text[/#{orientation}]")).to eq("<div style=\"text-align:#{orientation};\">Text</div>")
+      end
+    end
+
+    it 'justifies the text' do
+      expect(parse("[justify]Text[/justify]")).to eq("<div style=\"text-align:justify; text-justify: inter-word;\">Text</div>")
+    end
+
+    it 'shows an unordered list of items' do
+      expect(parse('[ul][li]List item[/li][li]Another list item[/li][/ul]')).to eq('<ul><li>List item</li><li>Another list item</li></ul>')
+    end
+
+    it 'shows a code' do
+      expect(parse('This is a [code]code[/code]')).to eq('This is a <pre>code</pre>')
+    end
+
+    it 'shows an ordered list of items' do
+      expect(parse('[ol][li]List item[/li][li]Another list item[/li][/ol]')).to eq('<ol><li>List item</li><li>Another list item</li></ol>')
+    end
+
+    it 'renders an image' do
+      expect(parse('[img]http://logo.jpg[/img]')).to eq('<img src="http://logo.jpg" />')
+      expect(parse('[img width=10px]http://logo.jpg[/img]')).to eq('<img src="http://logo.jpg" width="10px" />')
+      expect(parse('[img height=8px]http://logo.jpg[/img]')).to eq('<img src="http://logo.jpg" height="8px" />')
+      expect(parse('[img width=10px height=8px]http://logo.jpg[/img]')).to eq('<img src="http://logo.jpg" width="10px" height="8px" />')
+    end
+
+    it 'links to an url' do
+      expect(parse('[url]http://www.arenah.com.br/[/url]')).to eq('<a href="http://www.arenah.com.br/">http://www.arenah.com.br/</a>')
+      expect(parse('[url=http://www.arenah.com.br/]Google[/url]')).to eq('<a href="http://www.arenah.com.br/">Google</a>')
+    end
+
+    it 'renders a quote' do
+      expect(parse('[quote]BBCode is great[/quote]')).to eq('<blockquote>BBCode is great</blockquote>')
+    end
+
+    it 'changes the size of the text' do
+      expect(parse('[size=32]This is 32px[/size]')).to eq('<span style="font-size: 32px;">This is 32px</span>')
+    end
+
+    it 'changes the color of a text' do
+      expect(parse('[color=red]This is red[/color]')).to eq('<font color="red">This is red</font>')
+    end
+
+    it 'renders youtube videos' do
+      expected = '<iframe id="player" type="text/html" width="400" height="320" src="http://www.youtube.com/embed/E4Fbk52Mk1w?enablejsapi=1" frameborder="0"></iframe>'
+      expect(parse('[youtube]E4Fbk52Mk1w[/youtube]')).to eq(expected)
+    end
+
+    it 'renders youtube videos with different sizes' do
+      expected = '<iframe id="player" type="text/html" width="100" height="320" src="http://www.youtube.com/embed/E4Fbk52Mk1w?enablejsapi=1" frameborder="0"></iframe>'
+      expect(parse('[youtube width=100]E4Fbk52Mk1w[/youtube]')).to eq(expected)
+      expected = '<iframe id="player" type="text/html" width="400" height="20" src="http://www.youtube.com/embed/E4Fbk52Mk1w?enablejsapi=1" frameborder="0"></iframe>'
+      expect(parse('[youtube height=20]E4Fbk52Mk1w[/youtube]')).to eq(expected)
+      expected = '<iframe id="player" type="text/html" width="100" height="20" src="http://www.youtube.com/embed/E4Fbk52Mk1w?enablejsapi=1" frameborder="0"></iframe>'
+      expect(parse('[youtube width=100 height=20]E4Fbk52Mk1w[/youtube]')).to eq(expected)
+    end
+
+    it 'renders vimeo videos' do
+      expected = '<iframe src="http://player.vimeo.com/video/46141955?badge=0" width="400" height="320" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
+      expect(parse('[vimeo]http://vimeo.com/46141955[/vimeo]')).to eq(expected)
+    end
+
+    it 'renders vimeo videos with different sizes' do
+      expected = '<iframe src="http://player.vimeo.com/video/46141955?badge=0" width="100" height="320" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
+      expect(parse('[vimeo width=100]http://vimeo.com/46141955[/vimeo]')).to eq(expected)
+      expected = '<iframe src="http://player.vimeo.com/video/46141955?badge=0" width="400" height="20" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
+      expect(parse('[vimeo height=20]http://vimeo.com/46141955[/vimeo]')).to eq(expected)
+      expected = '<iframe src="http://player.vimeo.com/video/46141955?badge=0" width="100" height="20" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
+      expect(parse('[vimeo width=100 height=20]http://vimeo.com/46141955[/vimeo]')).to eq(expected)
+    end
+
+    it 'hides a message to the master' do
+      expect(parse('[master]Can I change my attribute?[/master]')).to eq('<div class="bbcode-master"><i class="fa fa-comments fa-2x"></i>Can I change my attribute?</div>')
+    end
+
+    it 'shows a dice roll' do
+      expect(parse('[dice color=green]1D100 = 73[/dice]')).to eq('<div class="bbcode-dice"><font color="green">1D100 = 73</font></div>')
+    end
+
+    it 'shows dices results' do
+      expected = 'dice result: <div class="bbcode-full-width"><div class="bbcode-dices"><kbd> title=</kbd><hr/>1d100 = 10</div></div>'
+      expect(parse('dice result: [dices=Title]1d100 = 10[/dices]')).to eq(expected)
+    end
+
+    it 'renders a line' do
+      expect(parse('[line][/line]')).to eq('<hr />')
+    end
+
+    it 'inserts a clear div' do
+      expect(parse('[clear][/clear]')).to eq('<div class="clear"></div>')
+    end
+
+    it 'hides a spoiler' do
+      expect(parse('[spoiler=Title]He dies in the end[/spoiler]')).to eq('<span class="bbcode-spoiler"><a href="javascript:;"><i class="fa fa-plus-square"></i> </a><span>He dies in the end</span></span>')
+    end
   end
 
   context 'nested tags' do
+    it 'applies both both and underline' do
+      expect(parse('This is an [b][u]underlined bold[/u][/b] text')).to eq('This is an <strong><u>underlined bold</u></strong> text')
+    end
+  end
+
+  context 'not closed tag' do
   end
 
   def parse(text)
