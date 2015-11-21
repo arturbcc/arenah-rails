bf3eb5b533d11f172254a115fbf7c50def98d44b
Artur <artur.prado@abril.com.br>
2015-11-21 09:39:55 -0200

    Emoji

--- spec/models/parsers/emoji_spec.rb
+++ spec/models/parsers/emoji_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 describe Parsers::Emoji do
+  context 'invalid emoji' do
+    it 'returns the original text' do
+      text = ':invald:'
+      expect(parse(text)).to eq(text)
+    end
+  end
 
+  context 'valid emoji' do
+    it 'returns an image with the emoji' do
+      text = ':laughing:'
+      expect(parse(text)).to include('laughing.png')
+    end
+  end
+
+  def parse(text)
+    Parsers::Emoji.parse(text)
+  end
 end
