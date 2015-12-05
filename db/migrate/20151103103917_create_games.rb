7957a417fc610665d429ec21178c01b7428f1ff1
Artur <arturbcc@yahoo.com.br>
2015-12-05 11:50:13 -0200

    Limit the size of name and short_description on the games table

--- db/migrate/20151103103917_create_games.rb
+++ db/migrate/20151103103917_create_games.rb
@@ -2,9 +2,9 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.integer :character_id, index: true, foreign_key: true, null: false
-      t.string :name, null: false
+      t.string :name, null: false, limit: 45
       t.string :subtitle
-      t.text :short_description, limit: 500
+      t.text :short_description, limit: 330
       t.text :description
       t.string :banner
       t.string :css
