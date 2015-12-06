1c8bdb01232dabe9173d454732c6c4cc602adb07
Artur <arturbcc@yahoo.com.br>
2015-12-05 22:52:15 -0200

    Include qtip and jquery-ui style

--- app/assets/javascripts/modules/tooltip.js
+++ app/assets/javascripts/modules/tooltip.js
@@ -1,3 +1,27 @@
 var Tooltip = function() {
-  $('[data-tooltip]').tooltip();
+  this.applyTooltip($('[data-tooltip]'));
+};
+
+var fn = Tooltip.prototype;
+
+fn.applyTooltip = function(elements) {
+  var self = this;
+
+  $.each(elements, function() {
+    var el = $(this);
+    el.qtip(self.options(el));
+  });
+};
+
+fn.options = function(el) {
+  return {
+    style: {
+      classes: el.data('classes') || 'qtip-tipsy',
+      width: el.data('width')
+    },
+    position: {
+      my: el.data('my') || 'top center',
+      at: el.data('at') || 'bottom center'
+    }
+  };
 };
