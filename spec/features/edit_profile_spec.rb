70804fcda1c17259cbf9ccc914648b1be4f78ee7
Artur <artur.prado@gmail.com>
2015-12-29 18:59:22 -0200

    Test profile changes with capybara

---
+++ spec/features/edit_profile_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+feature 'Edit Profile' do
+  let!(:user) { create(:user) }
+
+  scenario 'Open the profile and change user data', js: true do
+    sign_in user, true
+    visit root_path
+
+    click_link "Olá, #{user.name}"
+    wait_for_ajax
+
+    expect(page.body).to have_css('.modal-content')
+    expect(page.body).to have_css("input[value='#{user.name}']")
+
+    fill_in 'name', with: 'fake name'
+
+    click_button 'Salvar'
+    wait_for_ajax
+
+    expect(page.first('[data-target]').text).to eq "OLÁ, FAKE NAME"
+
+    click_link "Olá, fake name"
+    wait_for_ajax
+
+    fill_in 'name', with: 'John Doe'
+    fill_in 'current-password', with: user.password
+    fill_in 'new-password', with: '987654321'
+    fill_in 'password-confirmation', with: '987654321'
+
+    click_button 'Salvar'
+    wait_for_ajax
+
+    expect(page.first('[data-target]').text).to eq "OLÁ, JOHN DOE"
+  end
+end
