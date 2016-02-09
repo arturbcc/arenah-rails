283fa59cd05d31bf5dc71d48c71669945f22827c
Artur <artur.prado@gmail.com>
2016-02-09 18:14:02 -0200

    test tables

--- app/models/rpg/table.rb
+++ app/models/rpg/table.rb
@@ -3,5 +3,11 @@ module RPG
     include EmbeddedModel
 
     attr_accessor :name, :table_items, :unit
+
+    private
+
+    def nested_attributes
+      %w(table_items)
+    end
   end
 end
---
+++ spec/models/rpg/table_item_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe RPG::TableItem do
+  let(:table) { @system.tables.first }
+
+  before(:all) do
+    load_system
+  end
+
+  it 'serializes the 1st item on table `Carregar`' do
+    item = table.table_items.first
+    expect(item.key).to eq('1')
+    expect(item.value).to eq(15)
+  end
+
+  it 'serializes the 2st item on table `Carregar`' do
+    item = table.table_items[1]
+    expect(item.key).to eq('2')
+    expect(item.value).to eq(15)
+  end
+
+  it 'serializes the 3st item on table `Carregar`' do
+    item = table.table_items[2]
+    expect(item.key).to eq('3')
+    expect(item.value).to eq(20)
+  end
+end
