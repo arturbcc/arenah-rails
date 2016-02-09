9e0620c0ea647d10b8907313da78039db3b7cecf
Artur <artur.prado@gmail.com>
2016-02-09 18:03:04 -0200

    Test dice roll rules

--- spec/models/rpg/system_spec.rb
+++ spec/models/rpg/system_spec.rb
@@ -8,4 +8,22 @@ describe RPG::System do
   it 'serializes the system name' do
     expect(@system.name).to eq('Daemon')
   end
+
+  describe '#dice_roll_rules' do
+    it 'serializes all dice roll rules' do
+      expect(@system.dice_roll_rules.count).to eq(2)
+    end
+  end
+
+  describe '#dice_result_rules' do
+    it 'serializes all dice result rules' do
+      expect(@system.dice_result_rules.count).to eq(4)
+    end
+  end
+
+  describe '#tables' do
+    it 'serializes all tables' do
+      expect(@system.tables.count).to eq(4)
+    end
+  end
 end
