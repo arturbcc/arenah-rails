9732dd1b9e1504768cce363d7650de665950b434
Artur <artur.prado@gmail.com>
2016-02-13 18:14:02 -0200

    Fix wrong name of variable

--- app/controllers/game/sheet_controller.rb
+++ app/controllers/game/sheet_controller.rb
@@ -1,6 +1,6 @@
 class Game::SheetController < Game::BaseController
   def show
-    @character = Character.friendly.find(params[:character_slug])
+    character = Character.friendly.find(params[:character_slug])
     @sheet = SheetPresenter.new(character)
 
     render :show, layout: false
