e79f2b84b0992e17bc683a43cd807a86bd850a62
Artur <artur.prado@gmail.com>
2016-02-13 18:15:58 -0200

    Include attributes and methods on attributes group

--- app/models/rpg/attributes_group.rb
+++ app/models/rpg/attributes_group.rb
@@ -6,6 +6,11 @@ module RPG
       :show_on_posts, :order_on_posts, :attributes_points_formula,
       :group_points_formula, :character_attributes, :list, :equipments,
 
+      # The game master can deliberatedly add extra points to any attributes
+      # group. This attribute will be added on a character sheet, not on the
+      # game system.
+      :extra_points,
+
       # The type indicates what kind of attributes the group will hold.
       # Each attribute type has its own behavior. The valid types are:
       #
@@ -17,10 +22,29 @@ module RPG
       # source. The valid source types are:
       #
       # TODO: list all source types here`
-      :source_type
+      :source_type,
+
+      # These attributes are not stored in the database. They are calculated
+      # at the end of the sheet initialization, linking attributes that are
+      # based in others and calculating the formulas as well
+
+      # points will hold the result of the group_points_formula
+      :points
 
       def show_on_posts?
         !!show_on_posts
+      end
+
+      def used_points
+        @used_points ||= begin
+          return 0 if character_attributes.blank?
+
+          character_attributes.reduce(0) {|sum, attribute| sum += attribute.points.to_f }
+        end
+      end
+
+      def total_points
+        points.to_i + extra_points.to_i
       end
 
       private
