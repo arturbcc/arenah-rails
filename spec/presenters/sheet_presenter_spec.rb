e7f3d3392906cbac8e8fa91e633339cbfb3e91f6
Artur <artur.prado@gmail.com>
2016-02-28 09:47:01 -0300

    Include presenter test

---
+++ spec/presenters/sheet_presenter_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe SheetPresenter do
+  describe '#system' do
+    let(:user) { create(:user) }
+    let(:system) { @system = load_system }
+    let(:game) { create(:game, system: system.to_json) }
+    let(:sheet) { load_sheet('crossover', 'inuyasha') }
+    let(:character) { create(:character, game: game, user: user) }
+    let(:presenter) { SheetPresenter.new(character) }
+
+    it 'returns the system of the sheet' do
+      expect(presenter.system.name).to eq(system.name)
+    end
+
+    it 'lists the pages of the sheet' do
+      expect(presenter.pages.map(&:number)).to eq([1, 2, 3])
+    end
+
+    it 'lists the attributes on the header' do
+      allow(character).to receive(:sheet).and_return(sheet)
+      expect(presenter.header_attributes(page: 1).map(&:name)).to eq(['Dados'])
+    end
+
+    it 'lists the attributes on a column' do
+      allow(character).to receive(:sheet).and_return(sheet)
+      groups = presenter.column_attributes(page: 1, column: 1).map(&:name)
+      expect(groups).to eq(%w(Atributos Aprimoramentos Status))
+    end
+
+    it 'lists the attributes on the footer' do
+      allow(character).to receive(:sheet).and_return(sheet)
+      groups = presenter.footer_attributes(page: 1).map(&:name)
+      expect(groups).to be_empty
+    end
+  end
+end
