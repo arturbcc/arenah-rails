a2248c9fc8aae691da7542c5396a1a6f9c5ee297
Artur <artur.prado@gmail.com>
2016-03-02 08:00:24 -0300

    Add Palace sheet

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ strange = Character.create!(user: luisfelipe, name: 'Strange', avatar: 'strange.
 goku = Character.create!(user: willian, name: 'Goku', avatar: 'goku.jpg', post_count: 400, signature: '[img]http://stuffpoint.com/dragonball-z/image/107122-dragonball-z-dbz-banner.jpg[/img]', last_post_date: 1.day.ago, sheet: load_sheet('crossover', 'goku'))
 hiei = Character.create!(user: priscila, name: 'Hiei', avatar: 'hiei.png', post_count: 382, last_post_date: 4.days.ago, sheet: load_sheet('crossover', 'hiei'))
 oliver_tsubasa = Character.create!(user: nathalia, name: 'Oliver Tsubasa', avatar: 'oliver.jpg', post_count: 12, last_post_date: 7.days.ago, sheet: load_sheet('crossover', 'hiei'))
-palace = Character.create!(user: mayara, name: 'Palace', avatar: 'palace.jpg', post_count: 10, gender: 1, last_post_date: 10.days.ago, sheet: '{}')
+palace = Character.create!(user: mayara, name: 'Palace', avatar: 'palace.jpg', post_count: 10, gender: 1, last_post_date: 10.days.ago, sheet: load_sheet('crossover', 'palace'))
 coraline = Character.create!(user: pablo, name: 'Coraline', avatar: 'coraline.jpg', post_count: 50, gender: 1, last_post_date: 1.day.ago, sheet: load_sheet('crossover', 'coraline'))
 
 hank = Character.create!(user: luisfelipe, name: 'Hank', avatar: 'hank.png', post_count: 199, last_post_date: Time.now, sheet: '{}')
