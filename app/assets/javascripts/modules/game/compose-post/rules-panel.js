e5f8a26fe931bd77d788f18250ae31cf97788319
Artur <artur.prado@gmail.com>
2016-05-17 08:19:26 -0300

    Delegate rpg rules panel to correct js component

--- app/assets/javascripts/modules/game/compose-post/rules-panel.js
+++ app/assets/javascripts/modules/game/compose-post/rules-panel.js
@@ -1,14 +1,14 @@
 //TODO: Loose this coupling between rules-panel and compose-post-attribute-selection
 define('rules-panel', [], function() {
   function RulesPanel(options) {
-    this.onClearCallback = options['onClear'];
-    this.findAttribute = options['findAttribute'];
+    this.attributes = [];
 
     this.container = $('#collapseFour');
     this.attributesArea = $('#attributes-area');
     this.results = $('.tests-result');
     this.clearButton = this.container.find('.btn-danger');
     this.rollButton = this.container.find('btn-success');
+    this.attributeTemplate = $('.character-attribute-line-template');
 
     this._bindEvents();
   };
@@ -18,34 +18,90 @@ define('rules-panel', [], function() {
   fn._bindEvents = function() {
     var self = this;
 
-    $.proxyAll(this, 'clear', 'roll', 'recalculatePositions');
+    $.proxyAll(this,
+      '_clear',
+      '_recalculatePositions',
+      '_removeAttribute',
+      '_roll');
 
+    // Insert event observers inside this if. The rules panel is being loaded
+    // every time a sheet is opened and the events may be duplicated if any
+    // binding is made outside this if
     if (!this.container.data('loaded')) {
-      this.clearButton.on('click', this.clear);
+      this.clearButton.on('click', this._clear);
+
       this.attributesArea.sortable({
         stop: function (event, ui) {
           self.attributesArea.find('.odd').removeClass('odd');
           self.recalculatePositions();
         }
       });
+
+      this.attributesArea.on('click', '[data-remove-attribute]', this._removeAttribute);
     }
 
     this.container.attr('data-loaded', 'true');
   };
 
-  fn.clear = function() {
-    if (this.onClearCallback && typeof(this.onClearCallback) === 'function') {
-      this.onClearCallback();
+  fn.attributesCount = function() {
+    return this.attributes.length;
+  };
+
+  fn.addAttribute = function(data) {
+    var template = this.attributeTemplate.clone();
+
+    template.removeClass('character-attribute-line-template');
+    template.data({
+      'attribute-name': data.attributeName,
+      'character-name': data.name,
+      'character-id': data.id
+    });
+
+    data.position = this.attributes.length + 1;
+    data.originalPosition = data.position;
+
+    $('.attribute-position', template).html(data.position);
+    $('[data-original-position]', template).val(data.position);
+
+    var img = $('img', template);
+    img.attr('src', img.data('path').replace('AVATAR.png', data.avatar));
+    img.attr('alt', data.name);
+    img.attr('title', data.name + ' (' + data.attributeName + ')');
+    img.qtip({
+      style: { classes: 'qtip-tipsy' },
+      position: { my: 'top center', at: 'bottom center'}
+    });
+
+    $('.attribute', template).html(
+      "<option value='" + data.value + "'>" + data.value + " [Valor]</option> " +
+      "<option value='" + data.points + "'>" + data.points + " [Pontos]</option> "
+    );
+
+    if (this.attributes.length % 2 == 1) {
+      template.addClass('odd');
     }
+
+    if (this.attributes.length === 0) {
+      this.attributesArea.html(template);
+    } else {
+      this.attributesArea.append(template);
+    }
+
+    this.attributes.push(data);
+    this._updateBadgeCount();
+  };
+
+  fn.roll = function() {
+
+  };
+
+  fn._clear = function() {
+    this.attributes = [];
 
     this.attributesArea.html(this._emptyMessage());
     this.results.html('').css('background-color', 'transparent');
 
     $('.tests-result.on').removeClass('on');
-  };
-
-  fn.roll = function() {
-
   };
 
   fn._emptyMessage = function() {
@@ -57,22 +113,27 @@ define('rules-panel', [], function() {
       .append(' Escolha os atributos nas fichas');
   };
 
-  // fn._findAttribute = function(originalPosition) {
-  //   return $.grep(this.attributes, function (e) { return e.originalPosition == originalPosition; })[0];
-  // };
+  fn._updateBadgeCount = function() {
+    var badge = $('#rules-set-box-attributes'),
+        redBadgeClass = 'red-badge';
 
-  fn.recalculatePositions = function() {
+    if (this.attributes.length > 0) {
+      badge.addClass(redBadgeClass);
+    } else {
+      badge.removeClass(redBadgeClass);
+    }
+
+    badge.text(this.attributes.length);
+  };
+
+  fn._recalculatePositions = function() {
     var self = this,
         index = 0;
 
     $('#attributes-area .character-attribute-line').each(function (_, item) {
       var position = index + 1,
           originalPosition = $('[data-original-position]', item).val(),
-          attribute;
-
-      if (self.findAttribute && typeof(self.findAttribute) === 'function') {
-        attribute = self.findAttribute(originalPosition);
-      }
+          attribute = this._findAttribute(originalPosition);
 
       if (attribute) {
         attribute.position = position;
@@ -85,6 +146,30 @@ define('rules-panel', [], function() {
         index += 1;
       }
     });
+  };
+
+  fn._removeAttribute = function(e) {
+    var element = $(e.currentTarget),
+        line = element.parents('.character-attribute-line'),
+        originalPosition = $('[data-original-position]', line).val(),
+        attribute = this._findAttribute(originalPosition),
+        index = this.attributes.indexOf(attribute),
+        self = this;
+
+    line.fadeOut();
+    this.attributes.splice(index, 1);
+    this._updateBadgeCount();
+
+    if (this.attributes.length === 0) {
+      this._clear();
+    }
+
+    line.remove();
+    this._recalculatePositions();
+  };
+
+  fn._findAttribute = function(originalPosition) {
+    return $.grep(this.attributes, function (e) { return e.originalPosition == originalPosition; })[0];
   };
 
 // $("#collapseFour .btn-success").on("click", function () {
--- app/assets/javascripts/modules/game/sheet-attribute-selection/compose-post-attribute-selection.js
+++ app/assets/javascripts/modules/game/sheet-attribute-selection/compose-post-attribute-selection.js
@@ -1,25 +1,12 @@
 define('compose-post-attribute-selection', ['rules-panel'], function(RulesPanel) {
   function ComposePostAttributeSelection(game) {
-    var self = this;
-
+    this.rulesPanel = new RulesPanel();
     this.game = game;
-    this.attributes = [];
 
-    this.rulesPanel = new RulesPanel({
-      onClear: function() { self.attributes = []; },
-      findAttribute: $.proxy(self._findAttribute, this)
-    });
-
-    this._bindEvents();
+    $.proxyAll(this, 'selectionCallback');
   };
 
   var fn = ComposePostAttributeSelection.prototype;
-
-  fn._bindEvents = function() {
-    $.proxyAll(this, 'selectionCallback', '_removeAttribute', '_findAttribute');
-
-    $('#attributes-area').on('click', '[data-remove-attribute]', this._removeAttribute);
-  };
 
   fn.selectionCallback = function(attribute) {
     if ($('.character-attribute-line').length === 5) {
@@ -27,9 +14,13 @@ define('compose-post-attribute-selection', ['rules-panel'], function(RulesPanel)
     } else {
       var data = this._attributeData(attribute);
 
-      this._addAttribute(data);
-      $('#remote-modal').modal('hide');
+      this.rulesPanel.addAttribute(data);
+      this._closeAllModals();
     }
+  };
+
+  fn._closeAllModals = function() {
+    $('.modal').modal('hide');
   };
 
   fn._attributeData = function(attribute) {
@@ -50,85 +41,6 @@ define('compose-post-attribute-selection', ['rules-panel'], function(RulesPanel)
         };
 
     return data;
-  };
-
-  fn._addAttribute = function(data) {
-    var template = $('.character-attribute-line-template').clone();
-
-    template.removeClass('character-attribute-line-template');
-    template.data('attribute-name', data.attributeName);
-    template.data('character-name', data.name);
-    template.data('character-id', data.id);
-
-    data.position = this.attributes.length + 1;
-    data.originalPosition = data.position;
-
-    $('.attribute-position', template).html(data.position);
-    $('[data-original-position]', template).val(data.position);
-
-    var img = $('img', template);
-    img.attr('src', img.data('path').replace('AVATAR.png', data.avatar));
-    img.attr('alt', data.name);
-    img.attr('title', data.name + ' (' + data.attributeName + ')');
-    img.qtip({
-      style: { classes: 'qtip-tipsy' },
-      position: { my: 'top center', at: 'bottom center'}
-    });
-
-
-    $('.attribute', template).html(
-      "<option value='" + data.value + "'>" + data.value + " [Valor]</option> " +
-      "<option value='" + data.points + "'>" + data.points + " [Pontos]</option> "
-    );
-
-    if (this.attributes.length % 2 == 1)
-      template.addClass('odd');
-
-    if (this.attributes.length == 0) {
-      $('#attributes-area').html(template);
-    } else {
-      $('#attributes-area').append(template);
-    }
-
-    this.attributes.push(data);
-    this._attributesCount();
-  };
-
-  fn._attributesCount = function() {
-    var badge = $('#rules-set-box-attributes'),
-        redBadgeClass = 'red-badge';
-
-    if (this.attributes.length > 0) {
-      badge.addClass(redBadgeClass);
-    } else {
-      badge.removeClass(redBadgeClass);
-    }
-
-    badge.text(this.attributes.length);
-  };
-
-  fn._removeAttribute = function(e) {
-    var element = $(e.currentTarget),
-        line = element.parents('.character-attribute-line'),
-        originalPosition = $('[data-original-position]', line).val(),
-        attribute = this._findAttribute(originalPosition),
-        index = this.attributes.indexOf(attribute),
-        self = this;
-
-    line.fadeOut();
-    this.attributes.splice(index, 1);
-    this._attributesCount();
-
-    if (this.attributes.length === 0) {
-      this.rulesPanel.clear();
-    }
-
-    line.remove();
-    this.rulesPanel.recalculatePositions();
-  };
-
-  fn._findAttribute = function(originalPosition) {
-    return $.grep(this.attributes, function (e) { return e.originalPosition == originalPosition; })[0];
   };
 
   return ComposePostAttributeSelection;
