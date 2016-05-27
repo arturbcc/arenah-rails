32bf7872262e790f2fd28a4cc37b24672798b8de
Artur <artur.prado@gmail.com>
2016-05-27 13:01:49 -0300

    Clean topic and group forms

--- app/assets/javascripts/modules/game/topics/new-content.js
+++ app/assets/javascripts/modules/game/topics/new-content.js
@@ -1,43 +1,18 @@
 define('new-content', [], function() {
   function NewContent(container) {
     this.container = $(container);
-
-    this._bindEvents();
   };
 
   var fn = NewContent.prototype;
 
-  fn._bindEvents = function() {
-    $.proxyAll(this, '_onChangeRadioButton');
-
-    this.container
-      .find('[data-new-content-option]')
-      .on('change', this._onChangeRadioButton);
-  };
-
-  fn._onChangeRadioButton = function(e) {
-    var id = $(e.currentTarget).val(),
-        descriptionContainer = this.container.find('#description-container');
-
-    if (id === 'topic') {
-      descriptionContainer.removeClass('hidden');
-    } else if (id === 'category') {
-      descriptionContainer.addClass('hidden');
-    }
-  };
-
   fn._validForm = function() {
     var option = this.container.find('[data-new-content-option]:checked').val(),
         title = $.trim(this.container.find('#title').val()),
-        description = $.trim(this.container.find('#description').val()),
-        valid = false;
+        valid = true;
 
-    if (option === 'topic' && (title === '' || description === '')) {
-      NotyMessage.show('Todos os campos são obrigatórios', 2000);
-    } else if (option === 'category' && title === '') {
+    if (title === '') {
+      valid = false;
       NotyMessage.show('O título é obrigatório', 2000);
-    } else {
-      valid = true;
     }
 
     return valid;
@@ -46,8 +21,8 @@ define('new-content', [], function() {
   fn.save = function() {
     if (this._validForm()) {
       var activeGroup = $('[data-topic-group-id].active');
-
       this.container.find('#topic-group-id').val(activeGroup.data('topic-group-id'));
+
       this.container.submit();
     }
   };
