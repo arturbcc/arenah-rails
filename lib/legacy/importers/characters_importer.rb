321e964f46ecce9bf48f35dd0732a8210b7d4ef8
Artur <artur.prado@gmail.com>
2016-06-10 17:58:27 -0300

    Inactive users known to be test users

--- lib/legacy/importers/characters_importer.rb
+++ lib/legacy/importers/characters_importer.rb
@@ -5,12 +5,19 @@ require 'legacy/legacy_user'
 module Legacy
   module Importers
     class CharactersImporter
+      INACTIVE_NAMES_LIST = [
+        'Guest',
+        '??'
+      ].freeze
+
       def self.import(characters, users)
         puts '3. Creating characters...'
         bar = RakeProgressbar.new(characters.count)
         characters.each do |character|
           user = users.find { |user| user.id == character.user_id }
           character.create!(user.arenah_user)
+
+          character.arenah_character.update(status: 0) if INACTIVE_NAMES_LIST.include?(character.name)
 
           bar.inc
         end
