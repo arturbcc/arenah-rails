d65066970b3ecc9df3e103075c744c452bd27ea3
Artur <artur.prado@gmail.com>
2016-06-10 17:57:42 -0300

    Run before.js only if the layout if a game layout

--- app/assets/javascripts/pages/before.js
+++ app/assets/javascripts/pages/before.js
@@ -1,15 +1,17 @@
 page.at(':before', function() {
-  var Game = require('game'),
-      HeaderMenu = require('header-menu');
+  if ($('[data-game-layout]').length > 0) {
+    var Game = require('game'),
+        HeaderMenu = require('header-menu');
 
-  // Load the window.game with the current system in all game pages.
-  // the game_system_url is defined on the application.html.erb, inside the
-  // views/layout folder:
-  //
-  // <%= hidden_field :game_system, :url, value: game_system_path(@game) %>
-  if ($('#game_system_url').length > 0) {
-    window.game = new Game('#game_system_url');
+    // Load the window.game with the current system in all game pages.
+    // the game_system_url is defined on the application.html.erb, inside the
+    // views/layout folder:
+    //
+    // <%= hidden_field :game_system, :url, value: game_system_path(@game) %>
+    if ($('#game_system_url').length > 0) {
+      window.game = new Game('#game_system_url');
+    }
+
+    new HeaderMenu();
   }
-
-  new HeaderMenu();
 });
