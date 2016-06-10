3425c2b23f8853d83c60872e8c52a612f0b2a064
Artur <artur.prado@gmail.com>
2016-06-10 17:36:51 -0300

    Reorder topics and do not save subscription topic

--- lib/legacy/importers/topics_importer.rb
+++ lib/legacy/importers/topics_importer.rb
@@ -11,6 +11,8 @@ module Legacy
         bar = RakeProgressbar.new(topics.count)
 
         topics.each do |topic|
+          next if topic.topic_type == 1
+
           bar.inc
           game = games.find { |g| g.forum_id == topic.forum_id && g.valid? }
           group = game.try(:group_to_save_topics)
@@ -32,12 +34,27 @@ module Legacy
         end
 
         bar.finished
+        reorder_topics
+
         puts "#{Topic.count} topics created"
         puts ''
 
         puts "#{ignore_list.count} topics ignored:"
         ignore_list.each { |topic| puts topic }
 
+        puts ''
+      end
+
+      def self.reorder_topics
+        puts ''
+        Game.all.each do |game|
+          game.topic_groups.each do |group|
+            Topic.by_group_id(group.id).sort_by {|t| t.created_at}.reverse.each_with_index do |topic, index|
+              topic.update(position: index + 1)
+            end
+          end
+        end
+        puts 'Topics reordered'
         puts ''
       end
     end
