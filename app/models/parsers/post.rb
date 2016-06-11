ba5d2221de7750f2e074712f27880ed2ce0cda63
Artur <artur.prado@gmail.com>
2016-06-11 17:22:38 -0300

    Improve bbcode parser error message

--- app/models/parsers/post.rb
+++ app/models/parsers/post.rb
@@ -6,7 +6,8 @@ class Parsers::Post
   def self.parse(text)
     new(text, default_parsers).text.html_safe
   rescue
-    "** Há alguma coisa errada com suas tags. Verifique se todas estão sendo fechadas corretamente. **#{text.html_safe}"
+    error_message = new('[color=red]** Há alguma coisa errada com suas tags. Verifique se todas estão sendo fechadas corretamente **[/color]\n\n', default_parsers).text.html_safe
+    "#{error_message} #{text}".html_safe
   end
 
   private
