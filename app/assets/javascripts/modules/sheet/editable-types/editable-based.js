ec510e0dfb4c079a2f4f3dc95e5adc7d7285530f
Artur <artur.prado@gmail.com>
2018-01-13 22:40:52 -0200

    Remove unused code and call list onCall callback

--- app/assets/javascripts/modules/sheet/editable-types/editable-based.js
+++ app/assets/javascripts/modules/sheet/editable-types/editable-based.js
@@ -14,43 +14,6 @@ define('editable-based', ['transform', 'source-type-list'], function(Transform, 
 
     this.transform = this.transformer.toSpinner;
     data.attributesGroup.find('.based-warning').show();
-
-    if (sourceType === 'List') {
-      data.attributesGroup.find('.attributes-with-base').hide();
-      this._overrideListFunctions(data);
-    }
-  };
-
-  fn._overrideListFunctions = function (data) {
-    var list = $.extend({}, this.sourceTypeList);
-    list.initialize(data);
-
-    list.transform = function(editable) {
-      return false;
-    };
-
-    list.onNewItem = function(item) {
-      var input = item.find('.new-list-item input');
-
-      input.val(0);
-      this.transformer.inputToSpinner(input);
-      item.find('.ui-spinner-button').addClass('ui-state-default');
-
-      var abbreviation = item.data('attribute-abbreviation') || '0',
-          name = item.find('.smart-description').text();
-
-      item.find('.smart-description').text(name + ' (' + abbreviation + ')');
-    };
-
-    list.inputNewItem = function (container, points) {
-      content = '<span class="new-list-item editable-container editable-inline"><div><form class="form-inline editableform"><div class="control-group form-group"><div><div class="editable-input" style="position: relative;"><input data-mark="new-input" type="text" class="form-control input-sm" style="padding-right: 24px;"><span class="editable-clear-x" style="display: inline;"></span></div></div></div></form></div></span>';
-      container.html(content);
-      return false;
-    };
-
-    list.onSelectItemName = function (item) {
-      return item.Name + ' (' + (item.abbreviation ? item.abbreviation : '0') + ')';
-    }
   };
 
   fn.onCancel = function(data) {
@@ -64,7 +27,12 @@ define('editable-based', ['transform', 'source-type-list'], function(Transform, 
     data.attributesGroup.find('[data-editable-attribute]').each(function() {
       $(this).editable('hide');
     });
-  }
+
+    var sourceType = data.attributesGroup.attr('data-source-type');
+    if (sourceType === 'list') {
+      this.sourceTypeList.onCancel(data);
+    }
+  };
 
   return EditableBased;
 });
