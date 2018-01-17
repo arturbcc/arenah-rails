317fe8ee915c6bc0775cc1acf852a1966f79c2a9
Artur <artur.prado@gmail.com>
2018-01-17 09:33:09 -0200

    Add documentation about the sheet modes and pass the correct mode to the js

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,12 +2,44 @@
 
 class Character < ActiveRecord::Base
   extend FriendlyId
-  friendly_id :name, :use => :slugged
+  friendly_id :name, use: :slugged
 
   enum status: { inactive: 0, active: 1 }
   enum character_type: { pc: 0, npc: 1, game_master: 2 }
   enum gender:  { male: 0, female: 1 }
-  enum sheet_mode: { freemode: 0, gamemode: 1 }
+
+  # Public: the sheet mode tells how the character sheet can be modified.
+  #
+  # * free_mode: all modifications are allowed. It is very useful before the
+  #   character starts to play the game, where modifications will be made
+  #   frequently and have to be analyzed by the game master until the sheet
+  #   follows all the rules of the game. In this mode, the users can add more
+  #   attributes than they should (their attribute's group's points will be
+  #   negative) or the sheet can still be incomplete.
+  #
+  # * game_mode: it means the sheet is ready and the character can play the
+  #   game. He/she will not be able to descrease points in the attributes, nor
+  #   will be able to add attributes to groups or remove them from there.
+  #
+  #   In this mode, changes are only allowed if there are still remaining points
+  #   in a group, which is useful if the user gains points when they level up. A
+  #   pro tip here is to incentivate game masters to create attributes groups
+  #   with the total points based on the character level, so by simply
+  #   leveling up characters will give them points to change the sheet, the
+  #   scenario for which this mode was created. The user can change, but only
+  #   on level up scenarios.
+  #
+  #   Be aware that if the user does not use all points before entering in the
+  #   game mode he/she will be able to change the sheet during the game. If it
+  #   is not a desired scenario, the game master have to either prevent a user
+  #   to start without using all points or change the sheet to the
+  #   game_master_mode.
+  #
+  # * game_master_mode: no changes are allowed unless the logged user is the
+  #   game master. Not even the user that created the character will be able to
+  #   tamper with the sheet. It is used when the game master want to have full
+  #   controll of the changes.
+  enum sheet_mode: { free_mode: 0, game_mode: 1,  }
 
   belongs_to :user
   belongs_to :game
