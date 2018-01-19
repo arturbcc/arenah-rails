ca4334151efba7b865ddfbd1a5171d22a7c3741d
Artur <artur.prado@gmail.com>
2018-01-19 16:39:52 -0200

    Return to previous url ignoring urls made for ajax calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,15 +8,22 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :store_location
 
+  URL_IGNORE_LIST = %w(users sistema ficha danos iniciativas preview).freeze
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: User.permitted_parameters)
   end
 
+  # Private: Store last url as long as it isn't in the ignore list. The last url
+  # will be used to return the user to the previous page after login/logout.
+  #
+  # By default, routes exclusively created for building modals over Ajax calls
+  # should be included in the ignore list. If this rule is not respected, the
+  # users may face a problem like this: http://g.recordit.co/Yaac6X34z7.gif
   def store_location
-    unless request.fullpath =~ /\/users/ || request.fullpath =~ /\/sistema/
-      # store last url as long as it isn't a /users path or a /sistema path
+    unless request.fullpath =~ /\/#{URL_IGNORE_LIST.join('|')}/
       session[:previous_url] = request.fullpath
     end
   end
