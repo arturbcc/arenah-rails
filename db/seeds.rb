9583f8f2b5d08fb344389b785b755faf1ba22c23
Artur <artur.prado@gmail.com>
2018-01-22 13:29:58 -0200

    Load correct sheet on seed character

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,7 @@ inuyasha = Character.create!(user: mariana, name: 'Inuyasha', avatar: 'inuyasha.
 strange = Character.create!(user: luisfelipe, name: 'Strange', avatar: 'strange.jpg', post_count: 501, last_post_date: 1.day.ago, sheet_mode: 0, sheet: load_sheet('crossover', 'strange'))
 goku = Character.create!(user: willian, name: 'Goku', avatar: 'goku.jpg', post_count: 400, signature: '[img]http://stuffpoint.com/dragonball-z/image/107122-dragonball-z-dbz-banner.jpg[/img]', last_post_date: 1.day.ago, sheet: load_sheet('crossover', 'goku'))
 hiei = Character.create!(user: priscila, name: 'Hiei', avatar: 'hiei.png', post_count: 382, last_post_date: 4.days.ago, sheet: load_sheet('crossover', 'hiei'))
-oliver_tsubasa = Character.create!(user: nathalia, name: 'Oliver Tsubasa', avatar: 'oliver.jpg', post_count: 12, last_post_date: 7.days.ago, sheet: load_sheet('crossover', 'hiei'))
+oliver_tsubasa = Character.create!(user: nathalia, name: 'Oliver Tsubasa', avatar: 'oliver.jpg', post_count: 12, last_post_date: 7.days.ago, sheet: load_sheet('crossover', 'oliver_tsubasa'))
 palace = Character.create!(user: mayara, name: 'Palace', avatar: 'palace.jpg', post_count: 10, gender: 1, last_post_date: 10.days.ago, sheet: load_sheet('crossover', 'palace'))
 coraline = Character.create!(user: pablo, name: 'Coraline', avatar: 'coraline.jpg', post_count: 50, gender: 1, last_post_date: 1.day.ago, sheet: load_sheet('crossover', 'coraline'))
 
