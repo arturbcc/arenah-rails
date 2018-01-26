a6ac7dfec44da24e2548bbc5a281607378803408
Artur <artur.prado@gmail.com>
2018-01-26 11:45:39 -0200

    Fix bug that was loading always the same sheet

--- app/controllers/game/sheet_controller.rb
+++ app/controllers/game/sheet_controller.rb
@@ -2,18 +2,13 @@
 
 class Game::SheetController < Game::BaseController
   def show
-    @game = character.game
-    @sheet = SheetPresenter.new(character)
+    @character = Character.friendly.find(params[:character_slug])
+    @game = @character.game
+    @sheet = SheetPresenter.new(@character)
 
     render :show, layout: false
   end
 
   def update
-  end
-
-  private
-
-  def character
-    @character ||= Character.friendly.find(params[:character_slug])
   end
 end
